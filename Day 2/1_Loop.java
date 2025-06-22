6f270d2f23596452935790560a1574921045b4c2
Satyam <developer.satyam.mishra@proton.me>
2025-06-22 20:48:15 +0530

    LOOP

---
+++ Day 2/1_Loop.java
@@ -0,0 +1,10 @@
+class Loop{
+    public static void main(String args[]){
+        int a=5;
+        if(a > 11 ){
+            System.out.println("a is greater than 11");
+        }else{
+            System.out.println("a is less than 11");
+        }
+    }
+}
---
+++ Day 2/2_Loop.java
@@ -0,0 +1,14 @@
+class Loop{
+    public static void main(String args[]){
+        int a=20;
+        if(a > 11 ){
+             if(a>19){
+                System.out.println("a is greater than 19");
+             }
+        }else if ( a > 6){
+            System.out.println("a is greater than 2");
+        }else{
+            System.out.println("a is less than 11");
+        }
+    }
+}
