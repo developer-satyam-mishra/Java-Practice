d4e9aebb2d363b5ea2c4fbfad230e6eddf1758a4
Satyam <developer.satyam.mishra@proton.me>
2025-06-23 14:07:19 +0530

    Swapping

---
+++ Day 3/Swapping.java
@@ -0,0 +1,11 @@
+class Swapping{
+    public static void main(String args[]){
+        int a=10,b=30;
+        int t;
+        t=a;
+        a=b;
+        b=t;
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+    }
+}
