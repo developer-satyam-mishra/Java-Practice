00a76b02725aff77372279e89bd724073d179f73
Satyam <developer.satyam.mishra@proton.me>
2025-06-23 13:19:35 +0530

    Greater Number

---
+++ Day 3/GreaterNumber.java
@@ -0,0 +1,12 @@
+class GreaterNumber{
+    public static void main(String args[]){
+        int a=10,b=20;
+        if(a>b){
+            System.out.println("a is graeter than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+
+    }
+}
