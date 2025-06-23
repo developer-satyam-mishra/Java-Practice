07b769c805657fc54abbe21c6b009205e0f4d0bb
Satyam <developer.satyam.mishra@proton.me>
2025-06-23 12:05:40 +0530

    Learned For Loop

---
+++ Day 3/Table.java
@@ -0,0 +1,10 @@
+class Table{
+    public static void main(String args[]){
+        int a=2;
+        System.out.println("Table of "+a);
+     for(int i=1;i<=10;i++){
+        int table=a*i;
+        System.out.println(table);
+     }   
+    }
+}
