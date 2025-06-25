3557b0ebbc15744cd1236afbcc728e9d5421a312
Satyam <developer.satyam.mishra@proton.me>
2025-06-25 17:39:57 +0530

    Method2 prime number

--- Day 5/PrimeNumber.java
+++ Day 5/PrimeNumber.java
@@ -6,9 +6,10 @@ class PrimeNumber{
         int rem=0;
         if(number==1)
             System.out.println("Not a prime number");
-        for(int i = 2; i <number ; i++){
-            if(number % i== 0)
-            rem=rem+1;
+        for(int i = 2; i < number ; i++){
+            if(number % i== 0){
+                rem=rem+1;
+            }
         }
         if(rem==0){
             System.out.println(number+ " is a prime number");
@@ -18,3 +19,31 @@ class PrimeNumber{
         }
     }
 }
+
+
+
+
+class PrimeNumber2{
+    public static void main(String args[]){
+        boolean isPrime=true;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int number = scan.nextInt();
+        if (number==1){
+            System.out.println("Not a prime number" );
+        }else {
+            for(int i = 2; i < number; i++){
+                if(number % i == 0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.println("It is a prime number");
+            }
+            else{
+                System.out.println("Not a prime number");
+            }
+        }
+    }
+}        
