6da10728e2ac208929ee2076cffcb7326c6dba5d
Satyam <developer.satyam.mishra@proton.me>
2025-11-16 16:39:13 +0530

    Print integer entered by user

---
+++ Day 1/UserInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class UserInput{
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Integers are ");
+        int num=sc.nextInt();
+
+
+    }
+}
