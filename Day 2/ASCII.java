c3c4a2c0a9fe705d2b67eacffac99fb9b3e532b8
Satyam <developer.satyam.mishra@proton.me>
2025-11-18 21:47:32 +0530

    ASCII

---
+++ Day 2/ASCII.java
@@ -0,0 +1,8 @@
+class ASCII{
+    public static void main(String[]args){
+        char ch='S';
+        int ascii=(int) ch;
+        System.out.println("Ascii value of "+ ch +" is:" +ascii );
+        
+    }
+}
