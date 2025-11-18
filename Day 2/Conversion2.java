991202fa3f1d056cf7a462999d89539837395882
Satyam <developer.satyam.mishra@proton.me>
2025-11-18 20:57:55 +0530

    Conversion2

---
+++ Day 2/Conversion2.java
@@ -0,0 +1,7 @@
+class Conversion2{
+    public static void main(String []args){
+        long num=100;
+        int num2=(int) num;
+        System.out.println("Converted to "+num+" number");
+    }
+}
