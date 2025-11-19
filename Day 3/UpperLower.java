3e43392cc3e45ebe9b4a6bd3efde2cdcfd13e58b
Satyam <developer.satyam.mishra@proton.me>
2025-11-19 13:46:05 +0530

    UpperLower

---
+++ Day 3/UpperLower.java
@@ -0,0 +1,11 @@
+class UpperLower{
+    public static void main(String [] args){
+        char upper='A';
+        char lower= Character.toLowerCase(upper);
+        System.out.println(lower);
+        char lower1='h';
+        char upper2=Character.toUpperCase(lower1);
+        System.out.println(upper2);
+
+    }
+}
