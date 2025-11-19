ba9a0acbb28c0dddd981494791b7e1cca056a680
Satyam <developer.satyam.mishra@proton.me>
2025-11-19 12:43:47 +0530

    Conversion

---
+++ Day 3/Conversion4.java
@@ -0,0 +1,10 @@
+class Conversion4{
+    public static void main(String[]args){
+        int decimal=80;
+        char character=(char) decimal;
+        System.out.println(character);
+        char character1='S';
+        int decimal1=character1;
+        System.out.println(decimal1);
+    }
+}
