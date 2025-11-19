13e4b5e20426806f1fa31faaf9d43cf4ca9e8e77
Satyam <developer.satyam.mishra@proton.me>
2025-11-19 20:45:50 +0530

    UserSubtraction

---
+++ Day 3/UserSubtraction.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class UserSubtraction{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter your first number");
+        int a =scan.nextInt();
+        System.out.println("Enter your second number");
+        int b = scan.nextInt();
+        int c = a-b;
+        System.out.println("Subtraction of two numbers are " +c);
+    }
+}
