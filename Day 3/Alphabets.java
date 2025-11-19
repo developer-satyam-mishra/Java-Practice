12c3ca01d924933f0592cff1ee6a3e3ac2f5e727
Satyam <developer.satyam.mishra@proton.me>
2025-11-19 20:16:41 +0530

    Alphabet

---
+++ Day 3/Alphabets.java
@@ -0,0 +1,8 @@
+class Alphabets{
+    public static void main(String []args){
+        System.out.println("Alphabets are ");
+        for(int i=65;i<=90;i++){
+            System.out.print((char)i+", ");
+        }
+    }
+}
