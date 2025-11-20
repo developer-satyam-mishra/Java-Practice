092d1f53ce5140e9cdf7d8bbaf84dd0907ab5e5f
Satyam <developer.satyam.mishra@proton.me>
2025-11-20 13:36:03 +0530

    Division

---
+++ Day 4/Division.java
@@ -0,0 +1,8 @@
+class Division{
+    public static void main(String[]args){
+        int a = 63;
+        int b = 9;
+        int c = a/b;
+        System.out.println("Division of two numbers are " +c);
+    }
+}
