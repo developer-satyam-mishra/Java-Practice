2e6266a66b52ac98f792aad53eb0d28ba7742e93
Satyam <developer.satyam.mishra@proton.me>
2025-11-26 19:01:48 +0530

    AreaRectangle

---
+++ Day/AreaRectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class AreaRectangle{
+    public static void main(String[]args){
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Enter Your Length");
+        int a =scan.nextInt();
+         System.out.println("Enter Your Breath");
+         int b =scan.nextInt();
+         int c = a*b;
+         System.out.println("Area of Rectangle is "+c);;
+
+
+
+    }
+}
