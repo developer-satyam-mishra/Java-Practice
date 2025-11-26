a5fbe7046b563d6588a2db9b7e5116826244f5a4
Satyam <developer.satyam.mishra@proton.me>
2025-11-26 18:37:50 +0530

    AreaSquare

---
+++ Day/AreaSquare.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class AreaSquare{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter Your Number");
+        int a = scan.nextInt();
+        int c = a*a;
+        System.out.println("Area of square of your given number is " +c);
+        
+
+    }
+}
