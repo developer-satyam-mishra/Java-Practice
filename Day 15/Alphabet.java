c447abe0d8af48704b05b6251b5c47ae0f0ea5de
Satyam <developer.satyam.mishra@proton.me>
2025-12-03 14:28:41 +0530

    ALphabet

--- Day 15/Alphabet.java
+++ Day 15/Alphabet.java
@@ -4,7 +4,7 @@ class Alphabet{
         Scanner scan=new Scanner(System.in);
         System.out.print("Enter the alphabet : ");
         char alpha=scan.next().charAt(0);
-        if (alpha>='A' && alpha<='Z' || alpha>='a' && alpha<='z'){
+        if ((alpha>='A' && alpha<='Z') || (alpha>='a' && alpha<='z')){
             System.out.print("It is a Alphabet");
         }else{
             System.out.print("Not a alphabet");
