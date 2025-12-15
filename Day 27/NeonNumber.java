c74f9385820f742c094e7daa40f549e3b613522b
Satyam <developer.satyam.mishra@proton.me>
2025-12-15 22:33:05 +0530

    NeonNumber

---
+++ Day 27/NeonNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class NeonNumber{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter Your Number");
+        int num= scan.nextInt();
+        int temp = num*num;
+        int result=0;
+         while (temp != 0){
+            int rem = temp % 10 ;
+            result = result + rem;
+            temp = temp/10;
+        }
+        if (result==num){
+                System.out.print("It is Neon Number");
+            }else{
+                System.out.print("It is not a Neon Number");
+            }
+
+    }
+}
