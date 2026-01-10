015f05660f9ea2b104642b0957c07512b8e82887
Satyam <developer.satyam.mishra@proton.me>
2026-01-10 23:34:46 +0530

    PronicNumbers

---
+++ Day 52/PronicNumber2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class PronicNumber2{
+    public static void main (String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=scan.nextInt();
+        boolean isPronic=false;
+        for (int i = 0;i<=num;i++){
+            if(i*(i+1)==num){
+                isPronic=true;
+                break;
+            }
+        }
+        if(isPronic){
+            System.out.print("It is Pronic Number");
+        }else{
+            System.out.print("It is not Pronic Number");
+        }
+    }
+}
