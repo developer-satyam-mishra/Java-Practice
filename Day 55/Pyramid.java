a3429464eaf2aaa376677595de356c0666d570cf
Satyam <developer.satyam.mishra@proton.me>
2026-01-13 15:52:21 +0530

    Pyramid

---
+++ Day 55/Pyramid.java
@@ -0,0 +1,15 @@
+class Pyramid{
+    public static void main(String[]args){
+       int num = 4;
+        for (int i = num;i>=1;i--){
+            for (int j = 1;j<=num;j++){
+                if(j>=i){
+                    System.out.print("* ");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
