3efabee636fef25a7049f81efa3f85d85566cc1a
Satyam <developer.satyam.mishra@proton.me>
2026-01-16 12:46:25 +0530

    AlphabetG

---
+++ Day 58/AlphabetG.java
@@ -0,0 +1,15 @@
+class AlphabetG{
+    public static void main (String[]args){
+        int num = 6;
+        for (int i = 1;i<=num;i++){
+            for (int j = 1;j<=num;j++){
+                if(i==1 || i==num || j==1 || j==num && i>=(num/2) || i==(num/2) && j>=(num/2))  {
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
