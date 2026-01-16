68bd8a0aaea39803537bcf658479316678e09776
Satyam <developer.satyam.mishra@proton.me>
2026-01-16 12:00:52 +0530

    AlphabetC

---
+++ Day 58/AlphabetC.java
@@ -0,0 +1,15 @@
+class AlphabetC{
+    public static void main (String[]args){
+        int num = 6;
+        for (int i = 1;i<=num;i++){
+            for (int j = 1;j<=num;j++){
+                if(i==1 || j==1 || i==num )  {
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
