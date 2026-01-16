0b8fccfe215c3f2572d856ef314bd4cbf3f32bea
Satyam <developer.satyam.mishra@proton.me>
2026-01-16 11:18:52 +0530

    AlphabetsA

---
+++ Day 58/AlphabetsPattern.java
@@ -0,0 +1,15 @@
+class Alphabets{
+    public static void main (String[]args){
+        int num = 6;
+        for (int i = 1;i<=num;i++){
+            for (int j = 1;j<=num;j++){
+                if(i==1 || j==1 || i==(num/2) || j==6 ) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
