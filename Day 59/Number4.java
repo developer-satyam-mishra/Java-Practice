8a7eeee438511fb853e05abb3b766847b129b827
Satyam <developer.satyam.mishra@proton.me>
2026-01-17 22:07:10 +0530

    Number4

---
+++ Day 59/Number4.java
@@ -0,0 +1,15 @@
+class Number4{
+    public static void main(String[]args){
+        int num = 6;
+        for (int i = 1;i<=num;i++){
+            for (int j = 1;j<=num;j++){
+                if( i==(num/2) || j==num || j==1 && i<=(num/2))  {
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
