748eacc5d406eeae3d3b74d55557aba5a2e20e7c
Satyam <developer.satyam.mishra@proton.me>
2026-01-20 22:01:11 +0530

    LastElement

---
+++ Day 62/LastElement.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class LastElement{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num[]=new int[6];
+        for (int i =0;i<num.length;i++){
+            num[i]=scan.nextInt();
+        }
+        System.out.print("Last element of the array : " +num[num.length-1]);
+
+    }
+}
