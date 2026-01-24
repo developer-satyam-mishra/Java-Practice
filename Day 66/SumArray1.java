6744b3b4cc32738d043302b4fe0d35d770a10214
Satyam <developer.satyam.mishra@proton.me>
2026-01-24 23:13:33 +0530

    SumArray1

---
+++ Day 66/SumArray1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class SumArray1{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num[]=new int[6];
+        int sum=0;
+        for (int i =0;i<num.length;i++){
+            num[i]=scan.nextInt();
+            sum=sum+num[i];
+        }
+         
+        System.out.print("Sum of the array : " +sum);
+    }
+}
