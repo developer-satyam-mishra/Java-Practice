aa595fb3dbfd4458edad21dca59d4d3605f9d200
Satyam <developer.satyam.mishra@proton.me>
2026-01-26 13:16:23 +0530

    SortedArrayInsertionAtFirstIndex

---
+++ Day 68/SortedArrayInsertionAtFirstIndex.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class SortedArrayInsertionAtFirstIndex{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Number before insertion :");
+        int num[]=new int[5];
+        for (int i = 0; i<num.length-1;i++){
+            num[i]=scan.nextInt();
+        }
+        for (int i =num.length-1; i>0;i--){
+            num[i]=num[i-1];
+        }
+        System.out.print("Number after insertion :");
+         num[0]=0;
+         for (int i = 0; i<num.length;i++){
+            System.out.print(num[i]+",");
+         }
+
+    }
+}
