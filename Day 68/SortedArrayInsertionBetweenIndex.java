0e56007a3054a089db85d6b93f5a455d93016b82
Satyam <developer.satyam.mishra@proton.me>
2026-01-26 13:32:26 +0530

    Sorted Array Insertion Between Index

---
+++ Day 68/SortedArrayInsertionBetweenIndex.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class SortedArrayInsertionBetweenIndex{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Number before insertion :");
+        int num[]=new int[5];
+        for (int i = 0; i<num.length-1;i++){
+            num[i]=scan.nextInt();
+        }
+        System.out.print("Enter the index Number:");
+        int index=scan.nextInt();
+        for (int i =num.length-1; i>index;i--){
+            num[i]=num[i-1];
+        }
+        System.out.print("Number after insertion :");
+         num[index]=18;
+         for (int i = 0; i<num.length;i++){
+            System.out.print(num[i]+",");
+         }
+
+    }
+}
