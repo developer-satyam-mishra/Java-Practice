02e032c481e334a713857117247890fcdbf81bca
Satyam <developer.satyam.mishra@proton.me>
2026-01-26 12:51:22 +0530

    SortedArrayDeletionAtLastIndex

---
+++ Day 68/SortedArrayDeletionAtLastIndex.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class SortedArrayDeletionAtLastIndex{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Number before deletion :");
+        int num[]=new int[5];
+        for (int i = 0; i<num.length;i++){
+            num[i]=scan.nextInt();
+        }
+         System.out.print("Number after deletion :");
+         for (int i= 0;i<num.length-1;i++){
+            System.out.print(num[i]+",");
+         }
+    }
+}
