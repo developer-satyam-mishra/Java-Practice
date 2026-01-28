3b1572139873f47cb453f3a34bde175b8250c0af
Satyam <developer.satyam.mishra@proton.me>
2026-01-28 21:34:21 +0530

    Unsorted Insertion Between Index

--- Day 70/UnsortedInsertionBetweenIndex.java
+++ Day 70/UnsortedInsertionBetweenIndex.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class UnsortedInsertionAtFirstIndex{
+class UnsortedInsertionBetweenIndex{
     public static void main(String[] args){
         Scanner scan=new Scanner (System.in);
         System.out.print("Array before insertion : ");
@@ -7,12 +7,15 @@ class UnsortedInsertionAtFirstIndex{
         for(int i = 0;i<num.length-1;i++){
             num[i]=scan.nextInt();
         }
-        
+        System.out.print("Enter the  index number : ");
+        int index=scan.nextInt();
+
         System.out.print("Enter the number to insert : ");
         int num1=scan.nextInt();
 
-        num[num.length-1]=num[0];
-        num[0]=num1;
+        num[index]=num1;
+        num[num.length-1]=num[index];
+        
         
         System.out.print("Array after insertion : ");
         for(int i = 0;i<num.length;i++){
