517c8ea6915ed9337dfbfc14c3bc160a13087118
Satyam <developer.satyam.mishra@proton.me>
2026-02-08 22:32:52 +0530

    Duplicate Number

---
+++ Day 81/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.HashSet;
+class BinarySearch1 {
+    public static void main(String[]args) {
+        Scanner scan =new Scanner(System.in);
+        System.out.print("Array : ");
+        int nums[]=new int[6];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=scan.nextInt();
+        }
+        HashSet<Integer> set=new HashSet<>();
+        boolean isFound=false;
+        for(int num:nums){
+           if(set.contains(num)){
+            isFound =true;
+            break;
+        }
+        else{
+            set.add(num);
+        }
+       }
+       if(isFound){
+        System.out.print("It contains duplicate number");
+       }else{
+        System.out.print("It does not contain duplicate number");
+       }
+    }
+}
