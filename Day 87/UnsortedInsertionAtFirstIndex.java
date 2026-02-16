c2e9a21f3022cad5761f4eced3f1ab0561f03674
Satyam <developer.satyam.mishra@proton.me>
2026-02-16 21:05:14 +0530

    Unsorted Insertion At First Index

---
+++ Day 87/UnsortedInsertionAtFirstIndex.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class UnsortedInsertionAtFirstIndex{
+    public static void main(String[] args){
+        Scanner scan=new Scanner (System.in);
+        System.out.print("Array before insertion : ");
+        int num[]=new int[7];
+        for(int i = 0;i<num.length-1;i++){
+            num[i]=scan.nextInt();
+        }
+        
+        System.out.print("Enter the number to insert : ");
+        int num1=scan.nextInt();
+
+        num[num.length-1]=num[0];
+        num[0]=num1;
+        
+        System.out.print("Array after insertion : ");
+        for(int i = 0;i<num.length;i++){
+            System.out.print(num[i]+",");
+        }
+
+    }
+}
