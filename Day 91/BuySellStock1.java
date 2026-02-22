592643ad6c798ba31ecc89e74746ac90a66aa800
Satyam <developer.satyam.mishra@proton.me>
2026-02-22 22:38:33 +0530

    Buy and sell stock

---
+++ Day 91/BuySellStock1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class BuySellStock1 {
+    public static void main(String[]args) {
+        Scanner scan =new Scanner(System.in);
+        System.out.print("Array : ");
+        int prices[]=new int[6];
+        for(int i=0;i<prices.length;i++){
+            prices[i]=scan.nextInt();
+        }
+        int minPrices=Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int num:prices){
+            if(num<minPrices){
+                minPrices=num;
+            }
+            else{
+                maxProfit=Math.max(maxProfit,num-minPrices);
+            }
+        }
+        System.out.print("Best time to buy the stock is Rs. "+minPrices+" and to sell is Rs. "+maxProfit);
+    }
+}
