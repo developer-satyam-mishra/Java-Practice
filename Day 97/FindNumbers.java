7fdc6eb80b32019614674c72b0429ff3e82ac592
Satyam <developer.satyam.mishra@proton.me>
2026-02-28 20:54:37 +0530

    Find Missing Numbers in an Array

---
+++ Day 97/FindNumbers.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+class FindNumbers {
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+
+        List<Integer> missingNumbers = findDisappearedNumbers(nums);
+
+        System.out.println("Missing numbers are: " + missingNumbers);
+    }
+}
