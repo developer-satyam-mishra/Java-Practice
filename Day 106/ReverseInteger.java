cb3f7fb58d918b03a6e07b1589a31fdea8b23eb5
Satyam <developer.satyam.mishra@proton.me>
2026-03-09 21:12:13 +0530

    Reverse Integer

---
+++ Day 106/ReverseInteger.java
@@ -0,0 +1,26 @@
+class ReverseInteger {
+public static int reverse(int x) {
+    int result = 0;
+
+    while (x != 0) {
+        int remainder = x % 10;
+        x = x / 10;
+
+        // Overflow check
+        if (result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
+            return 0;
+        }
+
+        result = result * 10 + remainder;
+    }
+
+    return result;
+}
+
+public static void main(String[] args) {
+    int x = 123;
+    int ans = reverse(x);
+    System.out.println("Reversed number: " + ans);
+}
+
+}
